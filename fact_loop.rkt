07bbb29af1dc56ea37a1b673e6a305fc3eda9317
Robin Salkeld <robin.salkeld@gmail.com>
2014-08-25 13:47:04 -0700

    Converted fact_loop as well

--- fact_loop.rkt
+++ fact_loop.rkt
@@ -1,12 +1,12 @@
-(letC 'inputs (boxC (numC 0))
-      (letC 'fact (fileC "fact.rkt")
-            (appC (fileC "y-comb.rkt")
-                  (lamC 'fact_loop
-                        (labelC 'fact_loop
-                                (lamC 'dummy 
-                                      (letC 'in (readC "x")
-                                            (ifZeroC (idC 'in) 
-                                                     (idC 'in) 
-                                                     (seqC (writeC "fact(x)" (appC (idC 'fact) (idC 'in)))
-                                                           (seqC (setboxC (idC 'inputs) (plusC (unboxC (idC 'inputs)) (numC 1)))
-                                                                 (appC (idC 'fact_loop) (idC 'in))))))))))))
+(let ([inputs (box 0)])
+  (let ([fact (file "fact.rkt")])
+    ((file "y-comb.rkt")
+     (lambda (fact_loop)
+       (label fact_loop
+              (lambda (dummy)
+                (let ([in (read "x")])
+                  (if0 in 
+                       in 
+                       (seq (write "fact(x)" (fact in))
+                            (seq (set-box! inputs (+ (unbox inputs) 1))
+                                 (fact_loop dummy)))))))))))
--- miraj_parser.rkt
+++ miraj_parser.rkt
@@ -8,30 +8,42 @@
          (let ([head (car s)]
                [tail (cdr s)])
            (case head
+             ;; Numbers, arithmetic, and conditionals
              ['+
               (plusC (parse (list-ref tail 0)) (parse (list-ref tail 1)))]
              ['*
               (multC (parse (list-ref tail 0)) (parse (list-ref tail 1)))]
              ['if0
               (ifZeroC (parse (list-ref tail 0)) (parse (list-ref tail 1)) (parse (list-ref tail 2)))]
+             ;; Identifiers and functions
              ['lambda
               (lamC (car (list-ref tail 0)) (parse (list-ref tail 1)))]
              ['let
               (let ([defpair (car (list-ref tail 0))])
                 (letC (list-ref defpair 0) (parse (list-ref defpair 1)) (parse (list-ref tail 1))))]
+             ;; Boxes and sequencing
+             ['box
+              (boxC (parse (list-ref tail 0)))]
+             ['unbox
+              (unboxC (parse (list-ref tail 0)))]
+             ['set-box!
+              (setboxC (parse (list-ref tail 0)) (parse (list-ref tail 1)))]
              ['seq
               (seqC (parse (list-ref tail 0)) (parse (list-ref tail 1)))]
+             ;; Advice
              ['label
               (labelC (list-ref tail 0) (parse (list-ref tail 1)))]
              ['aroundapp
               (let ([defpair (car (list-ref tail 0))])
                 (aroundAppC (list-ref defpair 0) (parse (list-ref defpair 1)) (parse (list-ref tail 1))))]
+             ;; Input/Output
              ['read
               (readC (list-ref tail 0))]
              ['write
               (writeC (list-ref tail 0) (parse (list-ref tail 1)))]
              ['file
               (fileC (list-ref tail 0))]
+             ;; Application
              [else
               (appC (parse head) (parse (list-ref tail 0)))]))]
         [(number? s) (numC s)]
